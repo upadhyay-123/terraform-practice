9155582b3c25a55400aa73d8c30964dcb917e781
upadhyay-123 <162790127+upadhyay-123@users.noreply.github.com>
2025-06-22 15:41:49 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -5,23 +5,18 @@ required_providers {
         version = "~> 4.16"
 }
 }
-        required_version = ">= 1.2.0"
+required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
-locals {
-	instances = {"Devendra":"ami-0b0dcb5067f052a63","Braj":"ami-08c40ec9ead489470","Abrar":"ami-08c40ec9ead489470","Nimo":"ami-0b0dcb5067f052a63"}
-}
 
-resource "aws_instance" "aws_ec2_test" {
-        for_each = local.instances
-        ami = each.value
+resource "aws_instance" "my_ec2_instance" {
+        ami = "ami-0f918f7e67a3323f0"
         instance_type = "t2.micro"
         tags = {
-     Name = each.key
-  }
+                Name = "Terraform-Instance"
 }
-
+}
